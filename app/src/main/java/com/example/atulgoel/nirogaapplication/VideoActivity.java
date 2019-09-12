cbddbea06378bde946603b72521c18647f0fc8cb
atulgoel <atul.goel@nagarro.com>
2019-09-12 14:51:34 +0530

    Latest commit

--- app/src/main/java/com/example/atulgoel/nirogaapplication/VideoActivity.java
+++ app/src/main/java/com/example/atulgoel/nirogaapplication/VideoActivity.java
@@ -1,21 +1,12 @@
 package com.example.atulgoel.nirogaapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.youtube.player.YouTubeBaseActivity;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.example.myandroidlibrary.YouTubePlayerModule;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class VideoActivity extends AppCompatActivity {
-
-
     private YouTubePlayerView youTubeView;
     private String videoURL;
 
@@ -25,7 +16,6 @@ public class VideoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_video);
         videoURL = "https://youtu.be/aiQA1OzE7-Q";
         youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
-
         YouTubePlayerModule youtubePlayer = new YouTubePlayerModule(this, videoURL, youTubeView);
     }
 }
