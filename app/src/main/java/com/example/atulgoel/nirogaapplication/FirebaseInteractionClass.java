a8ec78577b8b66036bba2341e77d8b9ee6e1afef
atulgoel <atul.goel@nagarro.com>
2019-09-16 12:36:01 +0530

    Firebase Data Fetch

---
+++ app/src/main/java/com/example/atulgoel/nirogaapplication/FirebaseInteractionClass.java
@@ -0,0 +1,35 @@
+package com.example.atulgoel.nirogaapplication;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.myandroidlibrary.FirebaseInteraction;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+
+public class FirebaseInteractionClass extends AppCompatActivity {
+
+    ArrayList<String> mOptions;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mOptionsReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseInteraction firebaseResults = new FirebaseInteraction();
+        mOptions = new ArrayList<>();
+        mOptions = firebaseResults.getOptionsList("QuestionsData","second","options");
+       // Toast.makeText(this,mOptions.size(),Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ myandroidlibrary/src/main/java/com/example/myandroidlibrary/FirebaseInteraction.java
@@ -0,0 +1,45 @@
+package com.example.myandroidlibrary;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+
+public class FirebaseInteraction {
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mOptionsReference;
+    private ArrayList<String> mOptions;
+
+    public FirebaseInteraction(){
+            mDatabase = FirebaseDatabase.getInstance();
+    }
+
+    public ArrayList<String> getOptionsList(String dbName, String tableName, String keyName){
+        mOptionsReference = mDatabase.getReference(dbName)
+                .child(tableName)
+                .child(keyName);
+        mOptionsReference.addValueEventListener(optionsListener);
+        while(mOptions == null){
+        }
+        return mOptions;
+    }
+
+    ValueEventListener optionsListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            mOptions = new ArrayList<>();
+            for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                mOptions.add(postSnapshot.getValue().toString());
+            }
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
+}
