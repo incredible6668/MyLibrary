4ba2a718070ec77c1e4ce21f720be17cd280b0b9
atulgoel <atul.goel@nagarro.com>
2019-09-17 12:26:09 +0530

    Firebase Data Fetch

---
+++ myandroidlibrary/src/main/java/com/example/myandroidlibrary/FirebaseInteraction.java
@@ -0,0 +1,48 @@
+package com.example.myandroidlibrary;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+
+public class FirebaseInteraction {
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mOptionsReference;
+    private Boolean isOptionsListFilled;
+    private ArrayList<String> mOptions;
+
+    public FirebaseInteraction(){
+            mDatabase = FirebaseDatabase.getInstance();
+            mOptions = new ArrayList<>();
+            isOptionsListFilled = false;
+    }
+
+    public void showOptions(){
+        for(String option: mOptions){
+        }
+    }
+
+    public void getOptionsList(final ArrayList<String> options, String dbName, String tableName, String keyName){
+        mOptionsReference = mDatabase.getReference(dbName)
+                .child(tableName)
+                .child(keyName);
+        mOptionsReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    mOptions.add(postSnapshot.getValue().toString());
+                }
+                isOptionsListFilled = true;
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
